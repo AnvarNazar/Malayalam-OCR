47f94fd18dc48546a6332c2cb819bf4c73846a8e
Unknown <anvarn4@gmail.com>
2018-02-01 13:25:43 +0530

    findtext
    the identified text will be in integers. we need to convert the numbers to their corresponding malayalam unicode characters.

--- ocrdialog.cpp
+++ ocrdialog.cpp
@@ -16,24 +16,50 @@ OCRDialog::OCRDialog(QWidget *parent, cv::Mat classificationNumbers,
 
     cv::findContours(image, ptContours, v4iHierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
 
-    for(auto x:ptContours) {
+    for(unsigned int i = 0; i < ptContours.size(); i++) {
         ContourWithData contour;
-        contour.ptContour = x.data();
+        contour.ptContour = ptContours[i];
         contour.boundingRect = cv::boundingRect(contour.ptContour);
         contour.area = cv::contourArea(contour.ptContour);
         allContoursWithData.push_back(contour);
     }
 
     // find the valid contours;
-    for(auto x:allContoursWithData) {
-        if(x.checkIfContourIsValid(this->minContourArea)) { validContoursWithData.push_back(allContoursWithData); }
+    for(unsigned int i = 0; i < allContoursWithData.size(); i++) {
+        if(allContoursWithData[i].checkIfContourIsValid(this->minContourArea)) { validContoursWithData.push_back(allContoursWithData[i]);}
     }
 
     // sort contours from left to right
     std::sort(validContoursWithData.begin(), validContoursWithData.end(), ContourWithData::sorByBoundingRectXPosition);
 
+    // create KNearest
+    this->kNearest = cv::Ptr<cv::ml::KNearest>(cv::ml::KNearest::create());
+
     // train the ml algorithm
     kNearest->train(trainingImagesAsFlattenedFloats, cv::ml::ROW_SAMPLE, classificationNumbers);
+    findText();
+}
+
+void OCRDialog::findText()
+{
+    for(auto contourWithData:validContoursWithData) {
+        cv::rectangle(classificationNumbers,
+                      contourWithData.boundingRect,
+                      cv::Scalar(0, 255, 0), 2);
+
+        cv::Mat matROI = image(contourWithData.boundingRect);
+        cv::Mat matROIResized;
+        cv::resize(matROI, matROIResized, cv::Size(imageWidth, imageHeight));
+
+        cv::Mat matROIFloat;
+        matROIResized.convertTo(matROIFloat, CV_32FC1);
+        cv::Mat flattenedFloat = matROIFloat.reshape(1, 1);
+        cv::Mat currentChar(0, 0, CV_32F);
+        kNearest->findNearest(flattenedFloat, 1, currentChar);
+        float fCurrentChar = (float) currentChar.at<float>(0, 0);
+        strFinalStringNumbers = strFinalStringNumbers + char(int(fCurrentChar)) + " ";
+    }
+
 }
 
 OCRDialog::~OCRDialog()
